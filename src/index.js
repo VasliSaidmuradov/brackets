32439f77bee180bc9819666aca3461c9135ede91
Vasli Saidmuradov <vasli.said@gmail.com>
2020-03-19 19:36:47 +0000

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const config = bracketsConfig.map(el => el.join(''))
+
+	for (let i = 0; i < config.length; i++) {
+		while(1) {
+			if (str.includes(config[i])) {
+				str = str.replace(config[i], '')
+				i = 0
+			} else break
+		}
+	}
+
+	return !str ? true : false
 }
